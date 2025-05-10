fbea5227fa83b16e4d726f2d4c67a9beb9d55872
Alivan Wm <donikasinos66@gmail.com>
2025-05-10 10:04:12 +0700

    Push

--- db.js
+++ db.js
@@ -1,17 +1,19 @@
 const mysql = require("mysql2/promise");
 
+// Membuat koneksi pool
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT || 3306,
+});
+
 const connectToDatabase = async () => {
   try {
-    const connection = await mysql.createConnection({
-      host: process.env.MYSQL_HOST,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
-      port: process.env.MYSQL_PORT,
-    });
-
+    const connection = await pool.getConnection();
     console.log("Database connected successfully.");
-    return connection;
+    connection.release(); // Jangan lupa melepaskan koneksi kembali ke pool
   } catch (error) {
     console.error("Database connection failed: ", error);
     throw error;
