acef65d8405bf75ba5de575657daf635f388c848
Alivan Wm <donikasinos66@gmail.com>
2025-05-10 10:11:46 +0700

    Push

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 const mysql = require("mysql2/promise");
 
-// Membuat pool koneksi
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT || 3306,
+  host: process.env.MYSQL_HOST, // Gunakan host yang benar
+  user: process.env.MYSQL_USER, // User: root
+  password: process.env.MYSQL_PASSWORD, // Password sesuai dengan Railway
+  database: process.env.MYSQL_DATABASE, // Database: manajemenkost
+  port: process.env.MYSQL_PORT || 30166, // Port: 30166 sesuai dengan Railway
 });
 
 module.exports = pool;
